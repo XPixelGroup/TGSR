1dac85c4ebf208b7bce06704eb3f6cc67e01abfb
WenlongZhang0517 <zhangwenlong0517@gmail.com>
2024-03-15 13:49:40 +0800

    update

--- tgsr/models/hat_model.py
+++ tgsr/models/hat_model.py
@@ -1,10 +1,10 @@
 import torch
 from torch.nn import functional as F
 
-from mybasicsr.utils.registry import MODEL_REGISTRY
-from mybasicsr.models.sr_model import SRModel
-from mybasicsr.metrics import calculate_metric
-from mybasicsr.utils import imwrite, tensor2img
+from basicsr.utils.registry import MODEL_REGISTRY
+from basicsr.models.sr_model import SRModel
+from basicsr.metrics import calculate_metric
+from basicsr.utils import imwrite, tensor2img
 
 import math
 from tqdm import tqdm
